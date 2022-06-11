7898c411b9b5ce161a62848bc5553110eafafb29
ucanozyurteri <ozyurteri.ugur@gmail.com>
2022-06-11 12:58:54 -0500

    corrected password in module

--- DB/module/main.tf
+++ DB/module/main.tf
@@ -6,8 +6,7 @@ module db {
     db_deletion_protection = "false"
     db_database_version = "MYSQL_8_0"
     user_name = "team3"
-    user_host = "PUT-OUR-DOMAIN.COM"
-    user_password = "CHANGE-PASSWORD"
+    user_password = "passwordteam3"
 
 }
 
