7c17e8e496e0092ffb5bf281bfa88c494bd610e6
ferhat oguz <ferhatoguz33@gmail.com>
2022-06-11 00:10:49 -0500

    Changed variable in Load Balancer

--- Load Balancer.tf
+++ Load Balancer.tf
@@ -1,12 +1,12 @@
 resource "google_compute_forwarding_rule" "default" {
   name                  = "website-forwarding-rule"
-  region                = var.asg_config["region"]
+  region                = var.region
   port_range            = 80
   backend_service       = google_compute_region_backend_service.backend.id
 }
 resource "google_compute_region_backend_service" "backend" {
   name                  = "website-backend"
-  region                = var.asg_config["region"]
+  region                = var.region
   load_balancing_scheme = "EXTERNAL"
   health_checks         = [google_compute_region_health_check.hc.id]
 }
@@ -14,7 +14,7 @@ resource "google_compute_region_health_check" "hc" {
   name               = "check-website-backend"
   check_interval_sec = 1
   timeout_sec        = 1
-  region             = var.asg_config["region"]
+  region             = var.region
 
   tcp_health_check {
     port = "80"
