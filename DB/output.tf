2e9d0747c83ac4ff09b7c7267dfd1cb2f085ba58
ucanozyurteri <ozyurteri.ugur@gmail.com>
2022-06-13 13:40:55 -0500

    updated db

--- DB/output.tf
+++ DB/output.tf
@@ -1,15 +1,11 @@
 
 output endpoint {
-	# value = google_sql_database_instance.instance.resources[1].instances[0].attributes.public_ip_address
 	value = google_sql_database_instance.instance.public_ip_address
 }
 output user_name {
 	value = google_sql_user.users.name
 }
 
-output user_password {
-	value = google_sql_user.users.password
-}
 
 output db_name {
 	value = google_sql_database.database.name
@@ -18,36 +14,4 @@ output db_name {
 output host_name {
 	value = google_sql_user.users.host
 }
-
-
-# output db_name {
-
-# }
-
-# output db_region {
-
-# }
-
-# output db_tier {
-
-# }
-
-# output db_deletion_protection {
-
-# }
-
-# output db_database_version {
-
-# }
-
-# output user_name {
-
-# }
-
-# output user_host {
-
-# }
-
-
-
 
